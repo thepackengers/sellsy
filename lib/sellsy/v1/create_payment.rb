e1f6ab22860a6b17762c967d49a15f77ff18f1a8
Alexandre Bouvier <alexandre.b2506@gmail.com>
2023-05-11 16:55:50 +0200

    fix: update api paiment name

--- lib/sellsy/v1/create_payment.rb
+++ lib/sellsy/v1/create_payment.rb
@@ -4,7 +4,7 @@ module Sellsy
   module V1
     class CreatePayment < Base
       def call(body = {})
-        client.post_v1('Document.createPayment', body: body)
+        client.post_v1('Payments.create', body: body)
       end
     end
   end
